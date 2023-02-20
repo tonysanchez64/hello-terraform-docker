7491c050a355d9a918a592538e0efa7554d8a6da
antonio <tony64kx.me@gmail.com>
2023-02-20 09:27:12 +0100

    change jenkinsfile and output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
   description = "ID of the EC2 instance"
-  value       = aws_instance.app_server[count.index].id
+  value       = ["${aws_instance.app_server.*.id}"]
 }
 
-output "instance_public_ip" {
+/*output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.app_server[count.index].public_ip
-}
+} */
